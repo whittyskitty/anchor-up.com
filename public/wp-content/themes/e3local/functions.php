d8bd530f77d38e7553197a7fe57662e1622899d3
Scott Whitaker <scottwhitaker@Scotts-MacBook-Air.local>
2025-02-17 11:31:52 -0500

    ACF Locations1

--- public/wp-content/themes/e3local/functions.php
+++ public/wp-content/themes/e3local/functions.php
@@ -4,14 +4,14 @@
  * Theme setup.
  */
 
-$auto_include_dirs = ['classes'];
-foreach ($auto_include_dirs as $dir){
-	$dirpath = __DIR__.'/'.$dir;
-	$files = preg_grep('/^([^.])/', scandir($dirpath));
-	foreach ($files as $f){
-		require_once $dirpath.'/'.$f;
-	}
-}
+// $auto_include_dirs = ['classes'];
+// foreach ($auto_include_dirs as $dir){
+// 	$dirpath = __DIR__.'/'.$dir;
+// 	$files = preg_grep('/^([^.])/', scandir($dirpath));
+// 	foreach ($files as $f){
+// 		require_once $dirpath.'/'.$f;
+// 	}
+// }
 
 function tailpress_setup() {
 	add_theme_support( 'title-tag' );
@@ -200,3 +200,115 @@ function enqueue_digital_stylesheet() {
     );
 }
 add_action('wp_enqueue_scripts', 'enqueue_digital_stylesheet');
+
+
+add_action('init', 'locations_post_type');
+function locations_post_type() {
+    $labels = array(
+        'name' => 'Locations',
+        'singular_name' => 'Location',
+
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => 'locations', // Set the archive slug to 'locations'
+        'supports' => array('title', 'editor','thumbnail'), //'editor'
+        'menu_icon' => 'dashicons-location', // Set the Dashicon class for the menu icon
+
+    );
+
+    register_post_type('location', $args);
+    
+}
+
+/* Google Maps Locations Shortcode */
+add_shortcode('google_maps_locations', 'google_maps_shortcode');
+function google_maps_shortcode($atts) {
+    $atts = shortcode_atts(array(
+        'api_key' => 'AIzaSyD-YOe4j4bcSPO53h71D_NXDZwXYa8-kc8'
+    ), $atts);
+
+    ob_start();
+    ?>
+    <div id="map" style="height:500px"></div>
+    <script>
+        var map; // Declare map and infoWindow variables at a higher scope to access them later
+        var infoWindow;
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 9,
+                center: { lat: 40.7406, lng: -79.9959 } // Set the default center of the map
+            });
+
+            var markers = [];
+
+            var locations = [
+                <?php
+                $args = array(
+                    'post_type' => 'location', // Replace with your custom post type
+                    'posts_per_page' => -1
+                );
+                $query = new WP_Query($args);
+
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        $location_name = get_the_title();
+                        $location_address = get_field('location_address'); // Replace with your ACF field name
+                        $location_permalink = get_permalink();
+
+                        if ($location_address) {
+                            echo "{ name: '" . esc_js($location_name) . "', address: '" . esc_js($location_address) . "', permalink: '" . esc_js($location_permalink) . "'},";
+                        }
+                    }
+                }
+                wp_reset_postdata();
+                ?>
+            ];
+
+            // ...
+
+            for (var i = 0; i < locations.length; i++) {
+                var geocoder = new google.maps.Geocoder();
+                geocodeAddress(geocoder, map, locations[i], markers);
+            }
+
+            function geocodeAddress(geocoder, map, location, markers) {
+                geocoder.geocode({ address: location.address }, function(results, status) {
+                    if (status === 'OK') {
+                        var marker = new google.maps.Marker({
+                            map: map,
+                            position: results[0].geometry.location,
+                            title: location.name
+                        });
+
+                        markers.push(marker);
+
+                        var locationInfo = '<h3 class="pb-2" style="font-weight:500;">' + location.name + '</h3><p class="pb-1">' + location.address + '</p><p class="pb-1"><a href="https://www.google.com/maps/dir/?api=1&destination=' + encodeURIComponent(location.address) + '" target="_blank">Get Directions</a></p><a class="pb-1" href="' + location.permalink + '">View Location Details</a>';
+
+                        marker.addListener('click', function() {
+                            // Close the previously opened info window, if any
+                            if (infoWindow) {
+                                infoWindow.close();
+                            }
+
+                            infoWindow = new google.maps.InfoWindow({
+                                content: locationInfo
+                            });
+
+                            infoWindow.open(map, marker);
+                        });
+                    } else {
+                        console.log('Geocode was not successful for the following reason: ' + status);
+                    }
+                });
+            }
+        }
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=<?php echo $atts['api_key']; ?>&callback=initMap" async defer></script>
+    <?php
+    return ob_get_clean();
+}
